bced4ed59f0f75f4990a45f75b2f66e3ea70f7df
Rich Trott <rtrott@gmail.com>
2015-05-07 21:15:51 -0700

    runs callback

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+var stackexchange = require('stackexchange');
+var context = new stackexchange();
+
 var stackwatch = {};
 
-stackwatch.start = function() {
+stackwatch.check = function(options, callback) {  
+  var filter = {
+    tagged: 'node.js',
+    sort: 'activity',
+    order: 'asc'
+  };
+
+  context.questions.questions(filter, callback);
 };
 
 module.exports = stackwatch;
--- test/index.js
+++ test/index.js
@@ -10,11 +10,19 @@ var it = lab.test;
 var stackwatch = require('../index.js');
 
 describe('exports', function () {
-    it('should expose a start function', function (done) {
-        expect(typeof stackwatch.start).to.equal('function');
+    it('should expose a chck function', function (done) {
+        expect(typeof stackwatch.check).to.equal('function');
         done();
     });
 });
 
-describe('start()', function () {
+describe('check()', function () {
+    it('should run a callback', function (done) {
+        var verify = function (err, data) {
+            expect(err).to.be.null();
+            expect(data).to.be.an.object();
+            done();
+        };
+        stackwatch.check({}, verify);
+    });
 });
